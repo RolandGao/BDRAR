32d74d63c1001e5ab6827d44e64d8e8f92409aa2
Roland <41932972+RolandGao@users.noreply.github.com>
2020-07-17 15:43:56 -0700

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 # coding: utf-8
-sbu_training_root = './Datasets/SBU/SBUTrain4KRecoveredSmall'
-sbu_testing_root = './Datasets/SBU/SBU-Test'
+sbu_training_root = './SBU-shadow/SBUTrain4KRecoveredSmall'
+sbu_testing_root = './SBU-shadow/SBU-Test'
